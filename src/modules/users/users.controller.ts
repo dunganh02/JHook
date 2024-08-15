60b22b779751f3b0853090b3d32576c25c921227
ANHDUNG\padun <padungg8@gmail.com>
2024-08-15 15:21:45 +0700

    config router

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -17,6 +17,7 @@ export class UsersController {
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
+    console.log('>>> name', createUserDto.name);
     return this.usersService.create(createUserDto);
   }
 
